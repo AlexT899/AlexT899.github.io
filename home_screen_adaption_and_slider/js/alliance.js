29414fd2d88cc71426b39bf569e23fc57d2e8c26
AlexT899 <167863909+AlexT899@users.noreply.github.com>
2024-09-16 19:46:12 +0000

    Add files via upload

---
+++ home_screen_adaption_and_slider/js/alliance.js
@@ -0,0 +1,84 @@
+const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo";
+};
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+};
+const openMenu = (event) => {
+  //Функция открывания меню//
+menu.classList.add("is-open"); //вешает класс is-open//
+mMenuToggle.classList.add("close-menu");
+document.body.style.overflow = "hidden"; //запрещаем прокрутку сайта//
+lightModeOn();
+};
+const closeMenu = (event) => {
+  //Функция закрывания меню//
+menu.classList.remove("is-open"); //уберает класс is-open//
+mMenuToggle.classList.remove("close-menu");
+document.body.style.overflow = ""; //возвращает прокрутку сайта//
+lightModeOff();
+};
+window.addEventListener("scroll", () => {
+this.scrollY > 1 ? lightModeOn(): lightModeOff();
+});
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu(); 
+});
+
+const swiperSteps = new Swiper(".steps-slider", {
+   speed: 400,
+   slidesPerView: 1,
+   navigation: {
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev", 
+  },
+   breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const swiper = new Swiper(".features-slider",{
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev", 
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
